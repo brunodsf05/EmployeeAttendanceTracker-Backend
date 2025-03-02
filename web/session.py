7dba5f71438f194f39b1ea7a589cb2ed5d05608e
brunodsf05 <brunodsf05.profesional@gmail.com>
2025-03-02 19:25:20 +0100

    Feat: Roolback de refresh

--- web/session.py
+++ web/session.py
@@ -1,32 +1,20 @@
-from flask import request, make_response
-from flask_jwt_extended import decode_token, create_access_token, get_jwt_identity, exceptions
+from flask import request
+from flask_jwt_extended import decode_token, exceptions
 
 
 
 def is_authenticated():
-    """ Verifica si el usuario está autenticado y renueva el token si es necesario """
+    """ Verifica si el usuario está autenticado """
     try:
         token = request.cookies.get("access_token")
 
         if token:
-            decode_token(token)  # Verifica si el token es válido
+            # Intenta decodificar el token (si es válido)
+            decode_token(token)
             return True
 
-    except exceptions.ExpiredSignatureError:
-        # Si el token expiró, intentamos renovarlo con el refresh token
-        refresh_token = request.cookies.get("refresh_token")
-
-        if refresh_token:
-            try:
-                identity = decode_token(refresh_token)["sub"]  # Obtiene el usuario asociado al refresh token
-                new_access_token = create_access_token(identity=identity)
-
-                # Crear una respuesta con la nueva cookie
-                response = make_response(True)
-                response.set_cookie("access_token", new_access_token, httponly=True)
-                return True
-
-            except exceptions.JWTExtendedException:
-                pass  # Si hay algún error con el refresh token, el usuario no está autenticado
+    except exceptions.JWTExtendedException:
+        # Si hay cualquier error con el token, consideramos que no está autenticado
+        pass
 
     return False
