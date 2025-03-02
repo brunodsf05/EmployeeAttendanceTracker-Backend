2c1c021199364c3d7a72e16323bb40e4fe113d51
brunodsf05 <brunodsf05.profesional@gmail.com>
2025-03-02 20:02:45 +0100

    Fix

--- web/empresa.py
+++ web/empresa.py
@@ -14,14 +14,3 @@ class EmpresaForm(FlaskForm):
     longitud = FloatField("Longitud", validators=[DataRequired()])
     radio = IntegerField("Radio (en metros)", validators=[DataRequired()])
     submit = SubmitField("Guardar cambios")
-
-    def __init__(self, *args, **kwargs):
-        empresa = Empresa.get_all().first()
-
-        if empresa:
-            self.nombre.data = empresa.nombre
-            self.latitud.data = empresa.latitud
-            self.longitud.data = empresa.longitud
-            self.radio.data = empresa.radio
-
-        super().__init__(*args, **kwargs)
