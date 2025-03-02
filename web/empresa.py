58a280eb1b539c2f54edfd599b13e8aa843b9026
brunodsf05 <brunodsf05.profesional@gmail.com>
2025-03-02 20:20:59 +0100

    Fix: Validación de la empresa

--- web/empresa.py
+++ web/empresa.py
@@ -1,27 +1,24 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, FloatField, IntegerField
-from wtforms.validators import DataRequired
+from wtforms.validators import InputRequired, ValidationError
 
-from models import Empresa
+
+
+def validar_latitud(form, field):
+    if field.data < -90 or field.data > 90:
+        raise ValidationError('La latitud debe estar entre -90 y 90.')
+
+def validar_longitud(form, field):
+    if field.data < -180 or field.data > 180:
+        raise ValidationError('La longitud debe estar entre -180 y 180.')
 
 
 
 class EmpresaForm(FlaskForm):
     """Formulario para modificar los datos de una empresa. Por defecto viene con los campos ya rellenos, listos para ser cambiados."""
 
-    nombre = StringField("Nombre de la empresa", validators=[DataRequired()])
-    latitud = FloatField("Latitud", validators=[DataRequired()])
-    longitud = FloatField("Longitud", validators=[DataRequired()])
-    radio = IntegerField("Radio (en metros)", validators=[DataRequired()])
+    nombre = StringField("Nombre de la empresa", validators=[InputRequired()])
+    latitud = FloatField("Latitud", validators=[InputRequired(), validar_latitud])
+    longitud = FloatField("Longitud", validators=[InputRequired(), validar_longitud])
+    radio = IntegerField("Radio (en metros)", validators=[InputRequired()])
     submit = SubmitField("Guardar cambios")
-
-    def __init__(self, *args, **kwargs):
-        empresa = Empresa.get_first()
-
-        if empresa:
-            self.nombre.data = empresa.nombre
-            self.latitud.data = empresa.latitud
-            self.longitud.data = empresa.longitud
-            self.radio.data = empresa.radio
-
-        super().__init__(*args, **kwargs)
