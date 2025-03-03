e5ae58a02c8433d5e248b9e2cd762f7b5ebce5fe
brunodsf05 <brunodsf05.profesional@gmail.com>
2025-03-03 13:42:14 +0100

    Feat: Clase para que maneja el datetime actual de la aplicación

---
+++ mytime.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+
+
+class MyTime:
+    """ Define el día y horas actuales. Puedes alternar entre usar el tiempo actual o un tiempo estático personalizado (para fines de debug). """
+    _use_now = True
+    _custom_datetime = None
+    
+    @classmethod
+    def get(cls) -> datetime:
+        if cls._use_now or cls._custom_datetime is None:
+            return datetime.now()
+        return cls._custom_datetime
+    
+    @classmethod
+    def set(cls, use_now, custom_date=None, custom_time=None):
+        cls._use_now = use_now
+        if not use_now and custom_date and custom_time:
+            cls._custom_datetime = datetime.combine(custom_date, custom_time)
