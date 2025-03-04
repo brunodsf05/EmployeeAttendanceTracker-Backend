67bd10a98e9a1af4635ff39b08b891c0ab0588e5
brunodsf05 <brunodsf05.profesional@gmail.com>
2025-03-04 15:42:34 +0100

    Feat: Método para saber si el tiempo del servidor es automático o manual

--- mytime.py
+++ mytime.py
@@ -8,8 +8,14 @@ class MyTime:
     _custom_datetime = None
     
     @classmethod
+    def is_automatic(cls) -> bool:
+        """ Devuelve True si se debe usar la fecha y hora actuales, False si se debe usar una fecha y hora personalizada. """
+        return cls._use_now or cls._custom_datetime is None
+
+    @classmethod
     def get(cls) -> datetime:
-        if cls._use_now or cls._custom_datetime is None:
+        """ Devuelve la fecha y hora actuales según la configuración realizada con ``set()``. """
+        if cls.is_automatic():
             now = datetime.now()
             now += timedelta(hours=1) # Añadimos una hora para que coincida con la hora de España
             return now
@@ -18,6 +24,7 @@ class MyTime:
     
     @classmethod
     def set(cls, use_now, custom_date=None, custom_time=None):
+        """ Configura si se debe usar la fecha y hora actuales o una fecha y hora personalizada. """
         cls._use_now = use_now
         if not use_now and custom_date and custom_time:
             cls._custom_datetime = datetime.combine(custom_date, custom_time)
